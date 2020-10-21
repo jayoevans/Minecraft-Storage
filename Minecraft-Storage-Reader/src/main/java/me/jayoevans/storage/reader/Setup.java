625937493b5b1ba93821e3ac460d6a9343d834f9
Jay <jay.evans159@gmail.com>
2020-10-21 14:36:16 +1000

    Added default ops to server

---
+++ Minecraft-Storage-Reader/src/main/java/me/jayoevans/storage/reader/Setup.java
@@ -0,0 +1,15 @@
+package me.jayoevans.storage.reader;
+
+import me.jayoevans.storage.common.FileUtil;
+
+import java.io.File;
+
+public class Setup
+{
+    public static void main(String[] args) throws Exception
+    {
+        File source = new File("server");
+
+        FileUtil.zip(source, "dist" + File.separator + "server");
+    }
+}
