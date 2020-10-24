894f16a20a0e25c77acc40e7c6a7f1922cc1048d
jayoevans <jay.evans159@gmail.com>
2020-10-24 14:37:02 +1000

    Update StorageReader

--- Minecraft-Storage-Reader/src/main/java/me/jayoevans/storage/reader/StorageReader.java
+++ Minecraft-Storage-Reader/src/main/java/me/jayoevans/storage/reader/StorageReader.java
@@ -78,7 +78,7 @@ public class StorageReader
             File sourceFile = new File(this.serverDirectory, "world.tar.gz");
             this.storageManager.getObject(key, sourceFile);
 
-            FileUtil.unzip(sourceFile, this.serverDirectory);
+            FileUtil.unzip(sourceFile, new File(this.serverDirectory, "world"));
             sourceFile.delete();
         }
         catch (Exception e)
